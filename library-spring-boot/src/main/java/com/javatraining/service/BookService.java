8fcd543b7cc01268ba3c865f8ade6b17135648bb
UPADHYAY Aashish <aashish.upadhyay@soprasteria.com>
2022-10-03 12:22:09 +0530

    Done

---
+++ library-spring-boot/src/main/java/com/javatraining/service/BookService.java
@@ -0,0 +1,24 @@
+package com.javatraining.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.javatraining.entity.Book;
+import com.javatraining.repository.BookRepository;
+
+@Service
+public class BookService {
+	
+	@Autowired
+	BookRepository bookRepo;
+	
+	public List<Book> getAllBooks() {
+		List<Book> allBooks = (List<Book>) bookRepo.findAll();
+		allBooks.forEach(b -> b.setName(b.getName() + "-b"));
+		allBooks.forEach(b -> b.setAuthor(b.getAuthor() + "-a"));
+		return allBooks;
+		
+	}
+}
